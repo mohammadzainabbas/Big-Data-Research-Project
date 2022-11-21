256b7107ff2cd8ba322955865d25dfa2c65e09bc
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-21 16:14:11 +0100

    Updated code

--- src/st_graph_generation/generate_st_graph.py
+++ src/st_graph_generation/generate_st_graph.py
@@ -42,7 +42,9 @@ def get_frame(video_path: Union[Path, str]) -> Generator[str, None, None]: # htt
 def main() -> None:
     print_log("Starting the program")
 
-    model_path = join("")
+    model_path = join("/Users/mohammadzainabbas/Masters/CS/Big-Data-Research-Project/src/object_detection/yolov7_with_object_tracking/yolov7.pt")
+    model = load_model(model_path)
+    
 
 
 
