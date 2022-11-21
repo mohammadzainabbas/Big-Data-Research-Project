44195240dad8d9463f0802582a060ae4b2e5ce58
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-21 15:14:33 +0100

    Work in progress

--- src/st_graph_generation/generate_st_graph.py
+++ src/st_graph_generation/generate_st_graph.py
@@ -1,7 +1,13 @@
 from os import getcwd, listdir
 from os.path import join, isfile, isdir, exists, abspath, dirname, basename
+from typing import Generator
 
-def get_frame() -> 
+def get_frame() -> Generator[str, None, None]:
+    """Generator of all the frames in the current directory.
+    """
+    for file in listdir(getcwd()):
+        if isfile(file) and file.endswith('.png'):
+            yield file
 
 def main() -> None:
 
