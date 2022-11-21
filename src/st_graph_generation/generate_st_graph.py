0700ab76fb16bf95a8bf6e9bcd06abbd2dcb78fc
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-21 16:14:56 +0100

    Work in progress

--- src/st_graph_generation/generate_st_graph.py
+++ src/st_graph_generation/generate_st_graph.py
@@ -45,6 +45,8 @@ def main() -> None:
     print_log("Loading the model")
     model_path = join("/Users/mohammadzainabbas/Masters/CS/Big-Data-Research-Project/src/object_detection/yolov7_with_object_tracking/yolov7.pt")
     model = load_model(model_path)
+    print_log(f"{type(model) = }")
+    print_log("Model loaded")
 
 
 
