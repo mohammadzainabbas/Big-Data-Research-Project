f77323f6d713046844224f297d63e57197a3212f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-22 12:54:34 +0100

    Minor change

--- src/st_graph_generation/stg_utils.py
+++ src/st_graph_generation/stg_utils.py
@@ -9,12 +9,12 @@ def print_error(text: str) -> None: print(f"[ error ] {text}")
 def get_video_params(video_path: Union[Path, str]) -> int:
     if not exists(video_path): raise FileNotFoundError("Video file not found")
     cap = cv2.VideoCapture(video_path)
-    params = {
+    params = dict({
         "width": int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
         "height": int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
         "nframes": int(cap.get(cv2.CAP_PROP_FRAME_COUNT)),
         "fps": cap.get(cv2.CAP_PROP_FPS)
-    }
+    })
     cap.release()
     return params
 
