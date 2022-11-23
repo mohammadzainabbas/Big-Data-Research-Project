902d4f6b3577c2450f542d1de7700fd05871dd16
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-23 05:41:38 +0100

    Minor update

--- src/st_graph_generation/detect_with_networkx.py
+++ src/st_graph_generation/detect_with_networkx.py
@@ -8,6 +8,10 @@ import torch.backends.cudnn as cudnn
 from numpy import random
 from sys import path
 
+from typing import List, Tuple, Dict, Any, Union
+import networkx as nx
+from dataclasses import dataclass, field
+
 path.append("/Users/mohammadzainabbas/Masters/CS/Big-Data-Research-Project/src/object_detection/yolov7_with_object_tracking")
 
 from models.experimental import attempt_load
