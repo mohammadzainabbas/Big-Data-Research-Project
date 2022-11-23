423184c039a35dd3afae4d488b1f5364be5322f0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-23 06:48:41 +0100

    Work in progress

--- src/st_graph_generation/detect_with_networkx.py
+++ src/st_graph_generation/detect_with_networkx.py
@@ -80,7 +80,8 @@ def generate_spatial_graph(img, bbox, identities=None, categories=None, confiden
         id = int(identities[i]) if identities is not None else 0
         conf = confidences[i] if confidences is not None else 0
         class_name = names[cat]
-        graph.add_node(Node(id, x1, y1, x2, y2, conf, cat, class_name))
+
+        graph.add_node(Node(id, x1, y1, x2, y2, conf, cat, class_name, centroid))
 
         tl = opt.thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
 
@@ -96,12 +97,13 @@ def generate_spatial_graph(img, bbox, identities=None, categories=None, confiden
             c2 = x1 + t_size[0], y1 - t_size[1] - 3
             cv2.circle(img, (x1, y1), c2, color, -1, cv2.LINE_AA)  # filled
             cv2.putText(img, label, (x1, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
-    # For edges
+    
+    # Add edges to the graph
     for node1 in graph.nodes:
         for node2 in graph.nodes:
-            if node1.id != node2.id:
-                dist = ((node1.centroid[0] - node2.centroid[0])**2 + (node1.centroid[1] - node2.centroid[1])**2)**0.5
-                graph.add_edge(node1, node2, weight=dist)
+            if node1.id == node2.id: continue
+            graph.add_edge(node1, node2, weight=distance.euclidean(node1.centroid, node2.centroid))
+
 
     return img, graph
 
