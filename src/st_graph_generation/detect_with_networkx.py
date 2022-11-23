e6930a2e1c2fe9080cb88142f911b8b25c65655b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-23 06:50:25 +0100

    Minor

--- src/st_graph_generation/detect_with_networkx.py
+++ src/st_graph_generation/detect_with_networkx.py
@@ -189,6 +189,8 @@ def detect(save_img=False):
         if classify:
             pred = apply_classifier(pred, modelc, img, im0s)
 
+        graphs = list()
+
         # Process detections
         for i, det in enumerate(pred):  # detections per image
             if webcam:  # batch_size >= 1
