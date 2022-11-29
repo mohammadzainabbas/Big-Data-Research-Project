1fe06957497c26defa484ffafa6d443f38846eea
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-29 03:58:01 +0100

    Minor

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 # https://stackoverflow.com/a/50193944/6390175
 if __name__ == "__main__":
-    print(find_packages())
-    # setup(
-    #     name='bdrp',
-    #     version='1.0.0',
-    #     packages=find_packages(),
-    # )
+    setup(
+        name='bdrp',
+        version='1.0.0',
+        packages=find_packages(),
+    )
