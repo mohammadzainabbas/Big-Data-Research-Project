3bc5a2c9a109a1d9b5dac75719d74bc6d02e2c6a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-11-29 04:00:07 +0100

    Minor update

--- src/st_graph_generation/generate_st_graph.py
+++ src/st_graph_generation/generate_st_graph.py
@@ -1,22 +1,21 @@
-from os import getcwd, listdir
-from os.path import join, isfile, isdir, exists, abspath, dirname, basename
-from typing import Generator, Union, List, Tuple, Dict, Any
-
-import cv2
 import argparse
 import time
 from pathlib import Path
+from os.path import join
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
 from sys import path
+from scipy.spatial import distance
+
+from typing import List, Tuple, Dict, Any, Union
+import networkx as nx
 from dataclasses import dataclass, field
-from stg_utils import *
 
-path.append("/Users/mohammadzainabbas/Masters/CS/Big-Data-Research-Project/src/object_detection/yolov7_with_object_tracking")
+path.append("/Users/mohammadzainabbas/Masters/CS/Big-Data-Research-Project/src/object_detection/")
 
-from models.experimental import attempt_load, Ensemble
+from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, \
                 check_imshow, non_max_suppression, apply_classifier, \
@@ -27,67 +26,342 @@ from utils.torch_utils import select_device, load_classifier, time_synchronized,
 
 from sort import *
 
-def load_model(model_path: str) -> Ensemble:
-    if not exists(model_path): raise FileNotFoundError("Model file not found")
-    device = select_device()
-    return attempt_load(model_path, map_location=device), device  # load FP32 model
+def draw_boxes(img, bbox, identities=None, categories=None, confidences = None, names=None, colors = None):
+    """
+    Function to Draw Bounding boxes
+    """
+    for i, box in enumerate(bbox):
+        x1, y1, x2, y2 = [int(i) for i in box]
+        tl = opt.thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
 
-def get_frames(video_path: Union[Path, str]) -> Generator[str, None, None]: # https://stackoverflow.com/questions/42531143/how-to-type-hint-a-generator-in-python-3
-    if not exists(video_path): raise FileNotFoundError("Video file not found")
-    return LoadImages(video_path, img_size=640)
+        cat = int(categories[i]) if categories is not None else 0
+        id = int(identities[i]) if identities is not None else 0
+        # conf = confidences[i] if confidences is not None else 0
 
-def main() -> None:
-    print_log("Starting the program")
+        color = colors[cat]
+        
+        if not opt.nobbox:
+            cv2.rectangle(img, (x1, y1), (x2, y2), color, tl)
 
-    print_log("Loading the model")
-    model_path = join("/Users/mohammadzainabbas/Masters/CS/Big-Data-Research-Project/src/object_detection/yolov7_with_object_tracking/yolov7.pt")
-    imgsz = 640
+        if not opt.nolabel:
+            label = str(id) + ":"+ names[cat] if identities is not None else  f'{names[cat]} {confidences[i]:.2f}'
+            tf = max(tl - 1, 1)  # font thickness
+            t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+            c2 = x1 + t_size[0], y1 - t_size[1] - 3
+            cv2.rectangle(img, (x1, y1), c2, color, -1, cv2.LINE_AA)  # filled
+            cv2.putText(img, label, (x1, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+    return img
 
-    # """
-    # (1/10) * 30 fps = 3 frames (1 second)
-    # (1/10) * 302 frames = 30 frames (10 seconds)
-    # """
-    delta_time = (1, 10) # take 1 frame every 10 frames
+@dataclass(unsafe_hash=True)
+class Node:
+    id: int = field(default=0)
+    x1: int = field(default=0)
+    y1: int = field(default=0)
+    x2: int = field(default=0)
+    y2: int = field(default=0)
+    conf: float = field(default=float(0))
+    detclass: int = field(default=0)
+    class_name: str = field(default="")
+    centroid: tuple = field(default=(0, 0))
 
-    model, device = load_model(model_path)
+@dataclass(unsafe_hash=True)
+class Edge:
+    weight: Union[float, int] = field(default=0)
+
+def generate_spatial_graph(img, bbox, identities=None, categories=None, confidences = None, names=None, colors = None):
+    """
+    Construct a spatial graph from the bounding boxes, identities, categories, confidences, names and colors
+    """
+    graph = nx.Graph()
+    for i, box in enumerate(bbox):
+        x1, y1, x2, y2 = [int(i) for i in box]
+        centroid = ((x1 + x2) // 2, (y1 + y2) // 2)
+
+        cat = int(categories[i]) if categories is not None else 0
+        id = int(identities[i]) if identities is not None else 0
+        conf = confidences[i] if confidences is not None else 0
+        class_name = names[cat]
+
+        graph.add_node(Node(id, x1, y1, x2, y2, conf, cat, class_name, centroid))
+
+        tl = opt.thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+
+        color = colors[cat]
+        
+        if not opt.nobbox:
+            cv2.circle(img, centroid, 5 * tl, color, tl)
+
+        if not opt.nolabel:
+            label = f"Node({str(id)}): {names[cat]} {confidences[i]:.2f}" if identities is not None else  f'{names[cat]} {confidences[i]:.2f}'
+            tf = max(tl - 1, 1)  # font thickness
+            t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+            c2 = x1 + t_size[0], y1 - t_size[1] - 3
+            cv2.circle(img, centroid, tf * 5, color, -1, cv2.LINE_AA)  # filled
+            cv2.putText(img, label, (x1, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+    
+    line_color = [0, 0, 0]
+    line_thickness = 1
+    # Add edges to the graph
+    for node1 in graph.nodes:
+        for node2 in graph.nodes:
+            if node1.id == node2.id: continue
+            graph.add_edge(node1, node2, weight=distance.euclidean(node1.centroid, node2.centroid))
+            cv2.line(img, node1.centroid, node2.centroid, line_color, line_thickness)
+
+    return img, graph
+
+def detect(save_img=False):
+    source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, not opt.no_trace
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    if not opt.nosave:  
+        (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+    device = select_device(opt.device)
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
 
-    print_log("Model loaded")
+    if trace:
+        model = TracedModel(model, device, opt.img_size)
 
-    data_dir = join("/Users/mohammadzainabbas/Masters/CS/Big-Data-Research-Project/data")
-    video_path = join(data_dir, "street.mp4")
+    if half:
+        model.half()  # to FP16
 
-    dataset = get_frames(video_path)
-    params = get_video_params(video_path)
-    print_log(f"{len(dataset) = }")
-    print_log(f"{dataset.nframes = }")
-    print_log(f"{params = }")
-    i, total_processed = 0, 0
-    last_img = None
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    if webcam:
+        view_img = check_imshow()
+        cudnn.benchmark = True  # set True to speed up constant image size inference
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
+    else:
+        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    old_img_w = old_img_h = imgsz
+    old_img_b = 1
+
+    t0 = time.time()
+    ###################################
+    startTime = 0
+    ###################################
+    graphs = list()
     for path, img, im0s, vid_cap in dataset:
-        i += 1
-        print()
-        if i % int(delta_time[1]/delta_time[0]) != 0: continue
-        print_log(f"Processing ({i}/{params['nframes']}) frame ...\n")
-        is_first_processed_frame = last_img is None and total_processed == 0
-        
         img = torch.from_numpy(img).to(device)
-        img = img.float()
-        
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
 
-        total_processed += 1
-        last_img = img
+        # Warmup
+        if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+            old_img_b = img.shape[0]
+            old_img_h = img.shape[2]
+            old_img_w = img.shape[3]
+            for i in range(3):
+                model(img, augment=opt.augment)[0]
+
+        # Inference
+        t1 = time_synchronized()
+        pred = model(img, augment=opt.augment)[0]
+        t2 = time_synchronized()
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        t3 = time_synchronized()
+
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                dets_to_sort = np.empty((0,6))
+                # NOTE: We send in detected object class too
+                for x1,y1,x2,y2,conf,detclass in det.cpu().detach().numpy():
+                    dets_to_sort = np.vstack((dets_to_sort, 
+                                np.array([x1, y1, x2, y2, conf, detclass])))
+
+
+                if opt.track:
+  
+                    tracked_dets = sort_tracker.update(dets_to_sort, opt.unique_track_color)
+                    tracks =sort_tracker.getTrackers()
+
+                    # draw boxes for visualization
+                    if len(tracked_dets)>0:
+                        bbox_xyxy = tracked_dets[:,:4]
+                        identities = tracked_dets[:, 8]
+                        categories = tracked_dets[:, 4]
+                        confidences = None
+
+                        if opt.show_track:
+                            #loop over tracks
+                            for t, track in enumerate(tracks):
+                  
+                                track_color = colors[int(track.detclass)] if not opt.unique_track_color else sort_tracker.color_list[t]
+
+                                [cv2.line(im0, (int(track.centroidarr[i][0]),
+                                                int(track.centroidarr[i][1])), 
+                                                (int(track.centroidarr[i+1][0]),
+                                                int(track.centroidarr[i+1][1])),
+                                                track_color, thickness=opt.thickness) 
+                                                for i,_ in  enumerate(track.centroidarr) 
+                                                    if i < len(track.centroidarr)-1 ] 
+                else:
+                    bbox_xyxy = dets_to_sort[:,:4]
+                    identities = None
+                    categories = dets_to_sort[:, 5]
+                    confidences = dets_to_sort[:, 4]
+                
+                ######################################################
+                is_graph = True
+                graph = None
+                if is_graph:
+                    
+                    if opt.track:
+                        bbox_xyxy = tracked_dets[:,:4]
+                        identities = tracked_dets[:, 8]
+                        categories = tracked_dets[:, 4]
+                        confidences = tracked_dets[:, 5]
+                    else:
+                        bbox_xyxy = dets_to_sort[:,:4]
+                        identities = [f"{x}_{dataset.frame}" for x in range(len(dets_to_sort))]
+                        categories = dets_to_sort[:, 5]
+                        confidences = dets_to_sort[:, 4]
+                    
+                    im0, graph = generate_spatial_graph(im0, bbox_xyxy, identities, categories, confidences, names, colors)
+                ######################################################
+                # im0 = draw_boxes(im0, bbox_xyxy, identities, categories, confidences, names, colors)
+                
+            # Print time (inference + NMS)
+            print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+
+            # Stream results
+            ######################################################
+            if dataset.mode != 'image' and opt.show_fps:
+                currentTime = time.time()
+
+                fps = 1/(currentTime - startTime)
+                startTime = currentTime
+                cv2.putText(im0, "FPS: " + str(int(fps)), (20, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0),2)
+
+            #######################################################
+            if view_img:
+                cv2.imshow(str(p), im0)
+                cv2.waitKey(1)  # 1 millisecond
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                    print(f" The image with the result is saved in: {save_path}")
+                else:  # 'video' or 'stream'
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+                        if vid_cap:  # video
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        else:  # stream
+                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            save_path += '.mp4'
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer.write(im0)
+        graphs.append(graph)
+    if save_txt or save_img:
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        #print(f"Results saved to {save_dir}{s}")
+    print(f'Done. ({time.time() - t0:.3f}s)')
+    return graphs
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    model_path = join("/Users/mohammadzainabbas/Masters/CS/Big-Data-Research-Project/src/object_detection/yolov7.pt")
     
-    print_log(f"{total_processed = }")
+    parser.add_argument('--weights', nargs='+', type=str, default=model_path, help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+
+    parser.add_argument('--track', action='store_true', help='run tracking')
+    parser.add_argument('--show-track', action='store_true', help='show tracked path')
+    parser.add_argument('--show-fps', action='store_true', help='show fps')
+    parser.add_argument('--thickness', type=int, default=2, help='bounding box and font size thickness')
+    parser.add_argument('--seed', type=int, default=1, help='random seed to control bbox colors')
+    parser.add_argument('--nobbox', action='store_true', help='don`t show bounding box')
+    parser.add_argument('--nolabel', action='store_true', help='don`t show label')
+    parser.add_argument('--unique-track-color', action='store_true', help='show each track in unique color')
 
 
-    print_log(f"{type(dataset) = }")
+    opt = parser.parse_args()
+    print(opt)
+    np.random.seed(opt.seed)
 
+    sort_tracker = Sort(max_age=5,
+                       min_hits=2,
+                       iou_threshold=0.2) 
 
+    #check_requirements(exclude=('pycocotools', 'thop'))
 
-
-
-
-if __name__ == "__main__":
-    main()
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov7.pt']:
+                graphs = detect()
+                strip_optimizer(opt.weights)
+        else:
+            graphs = detect()
+            print(f"Total number of graphs generated: {len(graphs)}")
