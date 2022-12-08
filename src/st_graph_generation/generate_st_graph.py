3d112de1dea7489f7b126362ea7e67c7241593b5
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-12-08 14:16:37 +0100

    Updated code

--- src/st_graph_generation/generate_st_graph.py
+++ src/st_graph_generation/generate_st_graph.py
@@ -113,18 +113,15 @@ def generate_spatial_graph(img, bbox, identities=None, categories=None, confiden
 
     return img, graph
 
-def preprocess_frame(img, **kwargs):
+def generate_spatial_graph_for_frame(img):
+    """
+    Construct a spatial graph from a frame
+    """
     img = torch.from_numpy(img).to(device)
     img = img.half() if half else img.float()  # uint8 to fp16/32
     img /= 255.0  # 0 - 255 to 0.0 - 1.0
     if img.ndimension() == 3:
         img = img.unsqueeze(0)
-
-
-def generate_spatial_graph_for_frame(img):
-    """
-    Construct a spatial graph from a frame
-    """
 
 
 
