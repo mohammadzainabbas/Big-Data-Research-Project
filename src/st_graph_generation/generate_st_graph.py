9b7c87c5b938bb92426bbd49d04ab604c7ba5e5e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-12-08 13:53:56 +0100

    Minor update

--- src/st_graph_generation/generate_st_graph.py
+++ src/st_graph_generation/generate_st_graph.py
@@ -13,16 +13,18 @@ from typing import List, Tuple, Dict, Any, Union
 import networkx as nx
 from dataclasses import dataclass, field
 
-from src.object_detection.models.experimental import attempt_load
-from src.object_detection.utils.datasets import LoadStreams, LoadImages
-from src.object_detection.utils.general import check_img_size, check_requirements, \
+path.append("/Users/mohammadzainabbas/Masters/CS/Big-Data-Research-Project/src/object_detection/")
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, \
                 check_imshow, non_max_suppression, apply_classifier, \
                 scale_coords, xyxy2xywh, strip_optimizer, set_logging, \
                 increment_path
-from src.object_detection.utils.plots import plot_one_box
-from src.object_detection.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
-from src.object_detection.sort import *
+from sort import *
 
 def draw_boxes(img, bbox, identities=None, categories=None, confidences = None, names=None, colors = None):
     """
